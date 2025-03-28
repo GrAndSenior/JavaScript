88056c4c437dcbb652cd7841714922e1df24fb31
Grushovec Andrei <103903376+GrAndSenior@users.noreply.github.com>
2025-03-28 16:59:19 +0300

    Add files via upload

---
+++ 2 - PixelArt/designs.js
@@ -0,0 +1,54 @@
+// Select color input
+const colorInput = document.querySelector("#colorPicker");
+colorInput.addEventListener("change", colorSelection, false);
+
+const Canvas = document.querySelector("#pixelCanvas");
+
+function colorSelection(){
+  let colorChoice = colorInput.value;
+  Canvas.style.color = colorChoice;
+  return colorChoice;
+
+}
+
+
+
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+const submit = document.querySelector("[type='submit']");
+submit.addEventListener("click", makeGrid);
+
+function makeGrid(event) {
+  clearCanvas();
+  const r = document.getElementById("inputHeight").value;
+  const c = document.getElementById("inputWidth").value;
+  event.preventDefault();
+  for (let i =0; i < r; i++){
+    let createRow = document.querySelector("#pixelCanvas").insertRow();
+    for (let j = 0; j < c; j++){
+      createRow.insertCell();
+    }
+
+  }
+
+
+
+  document.getElementById('pixelCanvas').addEventListener("click", function(event) {
+  	
+  	if(event.target && event.target.nodeName == "TD") {
+  	
+      event.target.style.backgroundColor = colorSelection();
+  	}
+  });
+  console.log(r, c);
+
+}
+
+
+function clearCanvas(){
+        let tbl = document.getElementById("pixelCanvas");
+	while (tbl.tBodies[0].hasChildNodes()) {
+		tbl.tBodies[0].removeChild(tbl.tBodies[0].lastChild);
+	}
+}
